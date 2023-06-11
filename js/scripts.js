af187c5f36f1417e90d1f841a4328eab70626ecc
Adrian Zavala <zavaladrian@gmail.com>
2023-06-11 13:13:09 -0500

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,14 @@ $(function() {
         }
     });
 });
+
+
+
+$("#reserveButton").click(function(){
+    $('#reserveModal').modal('show');
+  });
+
+  $("#loginButton").click(function(){
+    $('#loginModal').modal('show');
+  });
+
